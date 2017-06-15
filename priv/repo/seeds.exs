3d598db328243a2770d0c00b83431592cedbaead
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-06-15 13:22:09 +0800

    Add seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -2,10 +2,31 @@
 #
 #     mix run priv/repo/seeds.exs
 #
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Cryptofolio.Repo.insert!(%Cryptofolio.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+
+alias Cryptofolio.Repo
+alias Cryptofolio.Dashboard.Trade
+alias Cryptofolio.Dashboard.Currency
+alias Cryptofolio.Dashboard.CurrencyTick
+
+if Mix.env == :dev do
+  # Any data for development goes here
+
+  currencies = [
+    Repo.insert!(%Currency{name: "Ripple", symbol: "XRP"}),
+    Repo.insert!(%Currency{name: "Bitcoin", symbol: "XBT"}),
+    Repo.insert!(%Currency{name: "Aeon", symbol: "AEON"}),
+    Repo.insert!(%Currency{name: "Golem", symbol: "Golem"}),
+    Repo.insert!(%Currency{name: "Siacoin", symbol: "SC"})
+  ]
+
+  Enum.each(currencies, fn(currency) ->
+    Enum.each(1..5, fn(i) ->
+      Repo.insert! %CurrencyTick{
+        cost_usd: 1 + (i * 5),
+        cost_btc: 1 + (i * 5),
+        last_updated: Ecto.DateTime.cast!({{2017, 1, 1}, {1, 5 + (i * 5), 0}}),
+        currency_id: currency.id
+      }
+    end)
+  end)
+end
