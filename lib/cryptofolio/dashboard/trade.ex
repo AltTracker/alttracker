8f931b7422657819e05953a5cdd5a14c926297bc
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-06-22 00:47:23 +0800

    Add datetime picker support to form

--- lib/cryptofolio/dashboard/trade.ex
+++ lib/cryptofolio/dashboard/trade.ex
@@ -1,3 +1,37 @@
+defmodule Cryptofolio.Dashboard.Trade.DateTime do
+  @behaviour Ecto.Type
+  def type, do: :naive_datetime
+
+  def cast(string) when is_binary(string) do
+    string = if string =~ " ", do: string, else: string <> " 00:00:00"
+
+    case NaiveDateTime.from_iso8601(string) do
+      {:ok, _} = ok -> ok
+      {:error, _} -> :error
+    end
+  end
+
+  def cast(_), do: :error
+
+  def load(term) do
+    load_naive_datetime(term)
+  end
+
+  defp load_naive_datetime({{year, month, day}, {hour, minute, second, microsecond}}),
+    do: {:ok, %NaiveDateTime{year: year, month: month, day: day,
+                             hour: hour, minute: minute, second: second, microsecond: {microsecond, 6}}}
+  defp load_naive_datetime({{year, month, day}, {hour, minute, second}}),
+    do: {:ok, %NaiveDateTime{year: year, month: month, day: day,
+                             hour: hour, minute: minute, second: second}}
+  defp load_naive_datetime(_),
+    do: :error
+
+  def dump(%NaiveDateTime{year: year, month: month, day: day,
+                            hour: hour, minute: minute, second: second, microsecond: {microsecond, _}}),
+    do: {:ok, {{year, month, day}, {hour, minute, second, microsecond}}}
+  def dump(_), do: :error
+end
+
 defmodule Cryptofolio.Dashboard.Trade do
   use Ecto.Schema
   import Ecto.Changeset
@@ -11,7 +45,7 @@ defmodule Cryptofolio.Dashboard.Trade do
 
     field :amount, :decimal
     field :cost, :decimal
-    field :date, :naive_datetime
+    field :date, Cryptofolio.Dashboard.Trade.DateTime
 
     timestamps()
   end
