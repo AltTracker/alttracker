bb3e544933ee45fd4096c0512f7ebf3ac516e2cd
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-06-24 16:45:01 +0800

    Improve onboarding when portfolio is empty

--- assets/js/components/portfolio.js
+++ assets/js/components/portfolio.js
@@ -16,63 +16,64 @@ if (chart) {
   const chartData = JSON.parse(chart.dataset.currencies)
 
   Highcharts.setOptions(theme)
-  const portchart = Highcharts.stockChart('portfolio-chart', {
-    chart: {
-      events: {
-        load: getSeriesData
-      }
-    },
-
-    legend: {
-      enabled: true
-    },
-
-    rangeSelector: {
-      selected: 1
-    },
-
-    tooltip: {
-      pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>${point.y}</b><br/>'
-    },
-
-    series: []
-  })
-   
-
-  async function getSeriesData () {
-    const uniqChartData = R.uniqBy(R.prop('symbol'))(chartData)
-
-    await Promise.all(
-      uniqChartData.map(async ({ name, symbol }) => {
-        try {
-          const getTicks = R.pipe(
-            JSON.parse,
-            R.prop('ticks'),
-            R.map(({ time, high }) => ([time * 1000, high]))
-          )
-
-          const resp = await fetch(`/api/coin_daily_history/${symbol}`)
-          const curr = await resp.text()
-
-          const displayName = `${name} (${symbol})`
-
-          portchart.addAxis({
-            id: displayName,
-            title: {
-              text: `${name}`
-            },
-            visible: false
-          })
-          portchart.addSeries({
-            name: displayName ,
-            data: getTicks(curr),
-            yAxis: displayName
-          })
-        } catch (e) {
-          console.error(e)
+  if (chartData.length > 0) {
+    const portchart = Highcharts.stockChart('portfolio-chart', {
+      chart: {
+        events: {
+          load: getSeriesData
         }
-      })
-    )
+      },
+
+      legend: {
+        enabled: true
+      },
+
+      rangeSelector: {
+        selected: 1
+      },
+
+      tooltip: {
+        pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>${point.y}</b><br/>'
+      },
+
+      series: []
+    })
+
+    async function getSeriesData () {
+      const uniqChartData = R.uniqBy(R.prop('symbol'))(chartData)
+
+      await Promise.all(
+        uniqChartData.map(async ({ name, symbol }) => {
+          try {
+            const getTicks = R.pipe(
+              JSON.parse,
+              R.prop('ticks'),
+              R.map(({ time, high }) => ([time * 1000, high]))
+            )
+
+            const resp = await fetch(`/api/coin_daily_history/${symbol}`)
+            const curr = await resp.text()
+
+            const displayName = `${name} (${symbol})`
+
+            portchart.addAxis({
+              id: displayName,
+              title: {
+                text: `${name}`
+              },
+              visible: false
+            })
+            portchart.addSeries({
+              name: displayName ,
+              data: getTicks(curr),
+              yAxis: displayName
+            })
+          } catch (e) {
+            console.error(e)
+          }
+        })
+      )
+    }
   }
 }
 
@@ -88,31 +89,33 @@ if (pie) {
     }))
   )
 
-  Highcharts.chart('portfolio-pie', {
-    title: false,
-    chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false,
-      type: 'pie'
-    },
-    tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: false
-        },
-        showInLegend: true
-      }
-    },
-    series: [{
-      name: 'Portfolio',
-      colorByPoint: true,
-      data: pieSeriesData
-    }]
-  })
+  if (pieSeriesData.length > 0) {
+    Highcharts.chart('portfolio-pie', {
+      title: false,
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: false
+          },
+          showInLegend: true
+        }
+      },
+      series: [{
+        name: 'Portfolio',
+        colorByPoint: true,
+        data: pieSeriesData
+      }]
+    })
+  }
 }
