8d7bdb99e88fab6221ad3f7822b53ea385400bd0
Nicolas Turlais <nicolas.turlais@gmail.com>
2017-06-30 10:52:09 +0200

    event handler as a named function

--- assets/js/components/coin-transactions.js
+++ assets/js/components/coin-transactions.js
@@ -1,15 +1,14 @@
+const toggleCoinTrades = function () {
+	const currencyId = this.getAttribute('data-currency-id')
+    const coinTrades = document.querySelectorAll('.coin-trade-' + currencyId)
+
+    coinTrades.forEach((coinTrade) => {
+      coinTrade.classList.toggle('visible')
+    })
+}
 
 const summaries = document.querySelectorAll('.coin-trades-summary')
 
 summaries.forEach((summary) => {
-  summary.addEventListener('click', function () {
-    const currencyId = this.getAttribute('data-currency-id')
-    const coinTrades = document.querySelectorAll('.coin-trade-' + currencyId)
-
-    coinTrades.forEach((coinTrade) => {
-      coinTrade.classList.toggle("visible")
-    })
-  })
+  summary.addEventListener('click', toggleCoinTrades)
 })
-
-
