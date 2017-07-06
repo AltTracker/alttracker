800f34d59f4b66b76f6477425aaaf9f071c6e16c
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-07-06 17:42:46 +0800

    List Added Fiats/Cryptocurrencies on seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -26,45 +26,77 @@ end
 
 alias Cryptofolio.Seeder.CoinList
 alias Cryptofolio.Repo
-alias Cryptofolio.Dashboard.Trade
 alias Cryptofolio.Dashboard.Currency
-alias Cryptofolio.Dashboard.CurrencyTick
 alias Cryptofolio.Schema.Fiat
 
 require Logger
 
-# Seed cryptocurrencies
-with {:ok, req} <- HTTPoison.get("https://www.cryptocompare.com/api/data/coinlist/"),
-     {:ok, json} <- Poison.decode(req.body),
-     {:ok, coins} <- CoinList.extract_coins_from_coinlist(json) do
-  Enum.each(coins, fn coin ->
-    changeset = %Currency{name: coin.name, symbol: coin.symbol, cryptocompare_id: coin.id, cryptocompare_image_url: coin.image_url}
-    |> Currency.changeset(%{})
+defmodule Cryptofolio.Seeder do
+  def seed_cryptocurrencies() do
+    with {:ok, req} <- HTTPoison.get("https://www.cryptocompare.com/api/data/coinlist/"),
+         {:ok, json} <- Poison.decode(req.body),
+         {:ok, coins} <- CoinList.extract_coins_from_coinlist(json) do
+      cryptos = Enum.map(coins, fn coin ->
+        changeset = %Currency{name: coin.name, symbol: coin.symbol, cryptocompare_id: coin.id, cryptocompare_image_url: coin.image_url}
+        |> Currency.changeset(%{})
 
-    case Repo.insert(changeset) do
-      {:ok, _ } -> {:ok}
-      {:error, error} -> Logger.error inspect(error)
+        case Repo.insert(changeset) do
+          {:ok, _ } -> %{ name: coin.name, symbol: coin.symbol }
+          {:error, _} -> nil
+        end
+      end)
+
+      {:ok, cryptos}
+    else {_, error} ->
+      {:error, error}
     end
-  end)
-else {_, error} ->
-  Logger.error "Seed script failed!"
-  Logger.error inspect(error)
+  end
+
+  def seed_fiats() do
+    root = "https://openexchangerates.org/api/currencies.json"
+    query = URI.encode_query(app_id: Application.get_env(:cryptofolio, :open_exchange_key))
+    currencies = "#{root}?#{query}"
+
+    with {:ok, req} <- HTTPoison.get(currencies),
+         {:ok, currs} <- Poison.decode(req.body) do 
+      fiats = Enum.map(currs, fn {k, v} ->
+        case Repo.insert(Fiat.changeset(%Fiat{name: v, symbol: k}, %{})) do
+          {:ok, _ } -> %{ name: v, symbol: k }
+          {:error, _} -> nil 
+        end
+      end)
+
+      {:ok, fiats}
+    else {_, error} ->
+      {:error, error}
+    end
+  end
 end
 
-# Seed fiat currencies
-root = "https://openexchangerates.org/api/currencies.json"
-query = URI.encode_query(app_id: Application.get_env(:cryptofolio, :open_exchange_key))
-currencies = "#{root}?#{query}"
+require Logger
+cryptos = Cryptofolio.Seeder.seed_cryptocurrencies()
+fiats = Cryptofolio.Seeder.seed_fiats()
 
-with {:ok, req} <- HTTPoison.get(currencies),
-     {:ok, currs} <- Poison.decode(req.body) do 
-  Enum.each(currs, fn {k, v} ->
-    case Repo.insert(Fiat.changeset(%Fiat{name: v, symbol: k}, %{})) do
-      {:ok, _ } -> {:ok}
-      {:error, error} -> Logger.error inspect(error)
+case cryptos do
+  {:ok, cryptocurrencies} ->
+    Logger.info "Seeding cryptocurrencies successful"
+    Logger.info "Currencies Added:"
+    Enum.each Enum.reject(cryptocurrencies, &is_nil/1), fn curr ->
+      Logger.info "#{curr.name} (#{curr.symbol})"
     end
-  end)
-else {_, error} ->
-  Logger.error "Seed script failed!"
-  Logger.error inspect(error)
+  {:error, error} ->
+    Logger.error "Seeding cryptocurrencies failed"
+    Logger.error error.reason
+end
+
+case fiats do
+  {:ok, fiats} ->
+    Logger.info "Seeding fiats successful"
+    Logger.info "Currencies Added:"
+    Enum.each Enum.reject(fiats, &is_nil/1), fn curr ->
+      Logger.info "#{curr.name} (#{curr.symbol})"
+    end
+  {:error, error} ->
+    Logger.error "Seeding fiats failed"
+    Logger.error error.reason
 end
