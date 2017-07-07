5667b765ec0de2cb01ca2b7ba835287c181453c8
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-07-07 11:23:33 +0800

    Add Portfolio Abilities

--- lib/cryptofolio/abilities.ex
+++ lib/cryptofolio/abilities.ex
@@ -1,10 +1,12 @@
 defimpl Canada.Can, for: Cryptofolio.User do
   alias Cryptofolio.User
   alias Cryptofolio.Dashboard.Trade
+  alias Cryptofolio.Dashboard.Portfolio
 
   def can?(%User{id: user_id}, action, %User{id: user_id}) when action in ~w(edit update), do: true
   def can?(%User{}, action, Trade) when action in ~w(index new create)a, do: true
-  def can?(%User{id: user_id}, _, %Trade{user_id: user_id}), do: true
+
+  def can?(%User{id: user_id}, action, %Portfolio{user_id: user_id}) when action in ~w(edit update delete)a, do: true
 
   def can?(_, _, _), do: false
 end
