c8c4c3302f54e16344c1ac875bb8a5239b3a3131
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-07-07 11:24:06 +0800

    Add active flag on portfolio schema

--- lib/cryptofolio/dashboard/portfolio.ex
+++ lib/cryptofolio/dashboard/portfolio.ex
@@ -9,16 +9,26 @@ defmodule Cryptofolio.Dashboard.Portfolio do
     field :name, :string
     field :is_public_ratio, :boolean
     field :is_public_all, :boolean
+    field :active, :boolean
 
     has_many :trades, Cryptofolio.Dashboard.Trade
     timestamps()
   end
 
   @doc false
+  def changeset(model, :deactivate) do
+    model
+    |> cast(%{ active: false }, [:active])
+  end
+
+  def changeset(model, :toggle_privacy) do
+    model
+    |> cast(%{ is_public_all: !model.is_public_all }, [:is_public_all])
+  end
+
   def changeset(%Portfolio{} = portfolio, attrs) do
     portfolio
-    |> cast(attrs, [:user])
-    |> cast_assoc(:user)
-    |> validate_required([:user])
+    |> cast(attrs, [:name, :is_public_all])
+    |> validate_required([:name])
   end
 end
